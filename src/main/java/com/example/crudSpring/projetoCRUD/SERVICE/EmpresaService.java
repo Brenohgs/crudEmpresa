41ed915fb4c45934f2415babd3fbba6762344140
Breno <brenohian@gmail.com>
2025-09-11 20:38:30 -0300

    Alterações service

--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,7 +1,10 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.REPOSITORY.EmpresaRepository;
 
 @Service
@@ -12,5 +15,20 @@ public class EmpresaService {
     public EmpresaService(EmpresaRepository ligacaoEmpresaRepository){
         empresaRepository = ligacaoEmpresaRepository;
     }
+
+    //SELECTS OU INSERTS OU ALTER TABLE OU DELETE
+    //"SELECT * FROM EMPRESA" - SELECT REALIZA LISTAGEM DE DADOS (retorna tudos os dados da empresa (* = tudo da tabela))
+
+    public List<Empresa> findAll(){
+        return empresaRepository.findAll();
+    }//select * from empresa
+
+    public Empresa cadastrarEmpresa(Empresa dadoEmpresa){
+            return empresaRepository.save(dadoEmpresa);
+    }
+
+    public void deletarEmpresa(Empresa dadoEmpresa){
+        empresaRepository.delete(dadoEmpresa);
+    }//Esse comando é "void" pq não precisa de nenhum rotorno
 
 }
